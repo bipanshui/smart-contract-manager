d0fb9ea09fa1669197e3ed7c9988d839cbd06ba9
bipanshu <bipanshukumar21@gmail.com>
2026-03-13 21:28:24 +0530

    database configured

--- src/main/java/com/bipanshu/scm/Application.java
+++ src/main/java/com/bipanshu/scm/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScmApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ScmApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
